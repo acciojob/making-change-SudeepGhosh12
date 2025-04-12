39131788830ed8c2557908782b66b22226fc1730
Sudeep Ghosh <133862916+SudeepGhosh12@users.noreply.github.com>
2025-04-12 16:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const makeChange = (c) => {
   // your name here
+	  c = parseInt(c); // Ensure c is an integer
+  const q = Math.floor(c / 25);
+  c %= 25;
+  const d = Math.floor(c / 10);
+  c %= 10;
+  const n = Math.floor(c / 5);
+  c %= 5;
+  const p = c;
+
+  return { q, d, n, p };
 };
 
 // Do not the change the code below
